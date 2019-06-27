95d9a5eaabf1cbfd3aeb1a6795d01fe63837089d
Jason Shultz <jasoneshultz@icloud.com>
2019-06-27 07:14:47 -0600

    still trying

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
 export const perPage = 4;
-export const prodEndpoint = `https://ap-yoga-prod.herokuapp.com/`;
+export const prodEndpoint = `https://ap-yoga-prod.herokuapp.com/`; // Heroku
+export const prodEndpoint = `http://165.227.209.31`; // DigitalOcean
