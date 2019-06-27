e767d0ef1937786d3469d26957325fabb3dcbf87
Jason Shultz <jasoneshultz@icloud.com>
2019-06-26 21:20:35 -0600

    debugging heroku

--- pages/permissions.js
+++ pages/permissions.js
@@ -5,9 +5,7 @@ import HasPermission from '../components/HasPermission';
 const PermissionsPage = props => (
   <div>
     <PleaseSignIn>
-      <HasPermission permissions="ADMIN,PERMISSIONUPDATE">
-        <Permissions />
-      </HasPermission>
+      <Permissions />
     </PleaseSignIn>
   </div>
 );
